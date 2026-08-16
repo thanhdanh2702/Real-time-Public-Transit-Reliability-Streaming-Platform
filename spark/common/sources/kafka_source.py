e45c427edc23bf9c612d6471be9eb357a8099281
DanhDik <thanhdanh1231102@gmail.com>
2026-08-16 11:03:08 +0700

    read topic from kafka

---
+++ spark/common/sources/kafka_source.py
@@ -0,0 +1,17 @@
+from pyspark.sql import DataFrame, SparkSession
+
+
+def read_kafka_stream(
+        spark: SparkSession,
+        bootstrap_servers: str,
+        topic: str,
+        starting_offsets: str = "latest"
+) -> DataFrame:
+    return (
+        spark.readStream
+        .format("kafka")
+        .option("bootstrap_servers", bootstrap_servers)
+        .option("subscribe", topic)
+        .option("startingOffsets", starting_offsets)
+        .load()
+    )
